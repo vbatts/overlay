cb6668d7bc6c8bbad264dd095fdb812d7cc0af5c
Vincent Batts <vbatts@hashbangbash.com>
2016-06-23 14:05:21 -0400

    WIP

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
 	"github.com/vbatts/overlay/mount"
 	"github.com/vbatts/overlay/state"
 )
@@ -20,7 +22,120 @@ var (
 	flDebug      = flag.Bool("debug", false, "enable debug output")
 )
 
+func actionUUID(context *cli.Context) error {
+	if context.Bool("gen") {
+		actionUUIDGen(context)
+		return nil
+	}
+	if context.Bool("unmount") {
+		for _, arg := range context.Args() {
+			if err := mount.UnmountPath(arg); err != nil {
+				log.Errorf("ERROR: unmounting %q: %s\n", arg, err)
+			} else {
+				fmt.Printf("unmounted %q\n", arg)
+			}
+		}
+	}
+	return nil
+}
+func actionUUIDGen(context *cli.Context) error {
+	fmt.Println(state.NewUUID())
+	return nil
+}
+func actionMounts(context *cli.Context) error {
+	return nil
+}
+func actionMountsMount(context *cli.Context) error {
+	return nil
+}
+func actionMountsUnmount(context *cli.Context) error {
+	return nil
+}
+func actionMountsCreate(context *cli.Context) error {
+	return nil
+}
+
 func main() {
+	app := cli.NewApp()
+	app.Name = "overlay"
+	app.Usage = "overlayfs helper for limited users"
+	app.Version = "0.1.0"
+	app.Authors = []cli.Author{{Name: "@vbatts", Email: "vbatts@thisco.de"}}
+	app.Before = preload
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "root",
+			Value: filepath.Join(os.Getenv("HOME"), ".local/share/overlay/"),
+			Usage: "Directory to story state of previous overlay mounts",
+		},
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "enable debug output",
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:  "mounts",
+			Usage: "operations dealing with mount paths",
+			Flags: []cli.Flag{},
+			Subcommands: []cli.Command{
+				{
+					Name:   "mount",
+					Usage:  "perform an overlay mount for the provided source directory",
+					Action: actionMountsMount,
+					Flags: []cli.Flag{
+						cli.StringFlag{
+							Name:  "src",
+							Usage: "source directory to overlay",
+						},
+						cli.StringFlag{
+							Name:  "target",
+							Usage: "destination to overlay to (default a subdirectory of the -root)",
+						},
+					},
+				},
+				{
+					Name:   "unmount",
+					Usage:  "unmount all provided paths",
+					Action: actionMountsUnmount,
+				},
+				{
+					Name:   "create",
+					Usage:  "prepare a new mount point",
+					Flags:  []cli.Flag{},
+					Action: actionMountsCreate,
+				},
+			},
+			Action: actionMounts,
+		},
+		{
+			Name:  "uuid",
+			Usage: "operations via UUID",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "unmount",
+					Usage: "unmount all provided UUIDs",
+				},
+				cli.BoolFlag{
+					Name:  "gen",
+					Usage: "output a generated UUID",
+				},
+			},
+			Subcommands: []cli.Command{
+				{
+					Name:   "gen",
+					Usage:  "output a generated UUID",
+					Action: actionUUIDGen,
+				},
+			},
+			Action: actionUUID,
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(0)
+
 	flag.Parse()
 	if *flDebug {
 		os.Setenv("DEBUG", "1")
@@ -128,4 +243,14 @@ func main() {
 		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+// do this before any subcommands
+func preload(context *cli.Context) error {
+	log.SetOutput(os.Stderr)
+	if context.GlobalBool("debug") {
+		os.Setenv("DEBUG", "1")
+		log.SetLevel(log.DebugLevel)
+	}
+	return nil
 }
